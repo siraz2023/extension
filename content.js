2114f968a814d1b72da6c7cdcba8865dad5ef7d4
siraz <143994491+siraz2023@users.noreply.github.com>
2024-07-09 17:16:56 +0530

    fix toggle button

--- content.js
+++ content.js
@@ -1,11 +1,26 @@
-const style = document.createElement('style');
-style.textContent = `
-  body {
-    background-color: black !important;
-    color: green !important;
+(function() {
+  const darkModeCSS = `
+    body {
+      background-color: black !important;
+      color: #ffffff !important;
+    }
+    a {
+      color: #fffff !important;
+    }
+    .dark-mode-toggle {
+      display: none !important;
+    }
+  `;
+
+  const styleElement = document.createElement('style');
+  styleElement.classList.add('dark-mode-style');
+  styleElement.textContent = darkModeCSS;
+
+  const existingStyleElement = document.querySelector('.dark-mode-style');
+
+  if (existingStyleElement) {
+    existingStyleElement.remove();
+  } else {
+    document.head.appendChild(styleElement);
   }
-  a {
-    color: red !important;
-  }
-`;
-document.head.appendChild(style);
+})();
--- popup.js
+++ popup.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-  document.getElementById('toggle').addEventListener('click', function () {
+  document.getElementById('toggle-dark-mode').addEventListener('click', function () {
       chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
           chrome.scripting.executeScript({
               target: { tabId: tabs[0].id },
